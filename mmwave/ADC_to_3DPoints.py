f9056fe460f2ec5325138c09b9f04b28e1abe665
yijiangw <yijiang.wang@hotmail.com>
2020-06-02 18:03:23 -0400

    little change to fix path of model

--- mmwave/ADC_to_3DPoints.py
+++ mmwave/ADC_to_3DPoints.py
@@ -11,7 +11,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from util import pm 
+from mmwave.util import pm 
 from scipy.ndimage import convolve1d
 # folder=sys.argv[1]
 
