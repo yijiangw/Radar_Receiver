27805ab45802eeeffc647fa29ebbe234f9a06378
yijiangw <yijiang.wang@hotmail.com>
2020-06-02 00:11:17 -0400

    update test.py to fit packet capture

--- mmwave/receiver_test.py
+++ mmwave/receiver_test.py
@@ -3,36 +3,36 @@ import numpy as np
 import threading
 import time
 
-a = adcCapThread(1,"adc")
+a = adcCapThread(1,"adc",receiverType="packet")
 a.start()
 time.sleep(1)
 counter = 0
 nlist = []
-lostPacket =[]
-FrameNuml =[]
+lostPacketList =[]
+ItemNumList =[]
 n = 0
 f = open("adc.bin", "wb")
 while True:
 # for i in range(1000):
-    readframe,FrameNum,lostPacketFlag=a.getFrame()
-    if FrameNum>0:
-        lostPacket.append(lostPacketFlag)
-        FrameNuml.append(FrameNum)
-        # print(lostPacketFlag)
+    readItem,ItemNum,lostPacketListFlag=a.getFrame()
+    if ItemNum>0:
+        lostPacketList.append(lostPacketListFlag)
+        ItemNumList.append(ItemNum)
+        # print(lostPacketListFlag)
         counter+=1
-        f.write(readframe.tobytes())
+        f.write(readItem.tobytes())
         if counter == 150:
             # np.save(str(n),nlist)
             counter = 0
             n+=1        
         
-    elif FrameNum==-1:
-        print(readframe)
-    elif FrameNum==-2:       
-        # print(readframe)
+    elif ItemNum==-1:
+        print(readItem)
+    elif ItemNum==-2:       
+        # print(readItem)
         time.sleep(0.04)
     if n>10:    
        a.whileSign = False 
-       print(lostPacket)
+       print(lostPacketList)
        f.close()
        break
